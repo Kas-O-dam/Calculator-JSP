3ca7798eeffd6c6bf1623654075386d2e52e0e1b
dinskoy_mapper926 <omapper@gmail.com>
2022-12-03 17:49:47 +0300

    Исправлены баги

--- decide.py
+++ decide.py
@@ -21,15 +21,20 @@ proto = {
 	'example': '',
 	'st_index': 0
 }
-def calc(input_ex):
+input_ex = ''
+counter = -1
+def calc(value):
+	global input_ex
 	global prior
 	global proto
-	counter=-1
+	global counter
+	input_ex = value
+	part = []
 	def get_passive():
 		global prior
 		string = ''
 		for idx in range(0,prior['op_index'])[::-1]:
-			#print('pidx: '+str(idx))
+			#print('pidx: '+str(idx)) #
 			sym = input_ex[idx]
 			try:
 				int(sym)
@@ -37,17 +42,17 @@ def calc(input_ex):
 			except:
 				op_dict = {'st_index': idx+1}
 				prior.update(op_dict)
-				#print('passive: ' + string[::-1])
+				#print('passive: ' + string[::-1]) #
 				return string[::-1]
 			if(idx==0):
 				op_dict = {'st_index': idx}
 				prior.update(op_dict)
-				#print('passive: ' + string[::-1])
-				return string[::-1]
+				#print('passive: ' + string[::-1]) #
+				return int(string[::-1])
 	def get_active():
 		string = ''
 		for idx in range(prior['op_index']+1,len(input_ex)):
-			#print('aidx: ' + str(idx))
+			#print('aidx: ' + str(idx)) #
 			sym = input_ex[idx]
 			try:
 				int(sym)
@@ -55,13 +60,13 @@ def calc(input_ex):
 			except:
 				op_dict = {'en_index': idx+1}
 				prior.update(op_dict)
-				#print('active: ' + string)
+				#print('active: ' + string) #
 				return string
 			if(idx==len(input_ex)-1):
 				op_dict = {'en_index': idx+2}
 				prior.update(op_dict)
-				#print('active: ' + string)
-				return string
+				#print('active: ' + string) #
+				return int(string)
 	def decide():
 		result=0
 		if(prior['operator']=='*'):
@@ -72,45 +77,45 @@ def calc(input_ex):
 			result = int(prior['passive'])+int(prior['active'])
 		if(prior['operator']=='-'):
 			result = int(prior['passive'])-int(prior['active'])
+		result = int(result)
 		#print('result: '+str(result))
 		return str(result)
-	def acpc():
-		op_dict = {'active': get_active()}
-		prior.update(op_dict)
-		op_dict = {'passive': get_passive()}
-		prior.update(op_dict)
-	for i in input_ex:
-		counter+=1
-		if(i=='*' or i=='/'):
-			op_dict = {'op_index': counter}
-			prior.update(op_dict)
-			acpc()
-			if(i=='*'):
-				op_dict = {'operator': '*'}
+	#for i in range(0,len(input_ex)):
+	#	if(input_ex[i]=='('):
+	#		for s in range(i,len(input_ex),1):
+	#			if(s==')'):
+	#				part.append(input_ex)
+	#				input_ex = input_ex[i+1:s]
+	#				calc(input_ex)
+	#				input_ex = part.pop()				
+	def searcher(op1, op2):
+		global input_ex
+		global counter
+		counter = -1
+		for i in input_ex:
+			counter+=1
+			if(i==op1 or i==op2):
+				if(counter == 0):
+					break
+				op_dict = {'op_index': counter}
 				prior.update(op_dict)
-			if(i=='/'):
-				op_dict = {'operator': '/'}
+				op_dict = {'active': get_active()}
 				prior.update(op_dict)
-				
-			input_ex = input_ex[0:prior['st_index']]+decide()+input_ex[prior['en_index']-1:len(input_ex)]
-			#print(prior['st_index'])
-			#print(prior['en_index'])
-			#print('ie: '+input_ex)
-	counter=-1
-	for i in input_ex:
-		counter+=1
-		if(i=='+' or i=='-'):
-			op_dict = {'op_index': counter}
-			prior.update(op_dict)
-			acpc()
-			if(i=='+'):
-				op_dict = {'operator': '+'}
+				op_dict = {'passive': get_passive()}
 				prior.update(op_dict)
-			if(i=='-'):
-				op_dict = {'operator': '-'}
-				prior.update(op_dict)
-			input_ex = input_ex[0:prior['st_index']]+decide()+input_ex[prior['en_index']-1:len(input_ex)]
-			#print(prior['st_index'])
-			#print(prior['en_index'])
-			#print('ie: '+input_ex)
+				if(i==op1):
+					op_dict = {'operator': op1}
+					prior.update(op_dict)
+				if(i==op2):
+					op_dict = {'operator': op2}
+					prior.update(op_dict)
+				input_ex = input_ex[0:prior['st_index']]+decide()+input_ex[prior['en_index']-1:len(input_ex)]
+				#print(prior['st_index'])
+				#print(prior['en_index'])
+				#print('ie: '+input_ex)
+				if(input_ex.find(op1) or input_ex.find(op2)):
+					searcher(op1, op2)
+				break
+	searcher('*', '/')
+	searcher('+', '-')
 	return input_ex
