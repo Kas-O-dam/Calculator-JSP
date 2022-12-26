30868c40476a7a13fa3cd714db2700fad46f4ac2
dinskoy_mapper926 <omapper@gmail.com>
2022-12-26 21:01:54 +0300

    Танцуем и поём 🕺
    Калькулятор на ЖабаСкрипте готов, скобки полноценно работают (если с этим ещё баги - сообщите). Заключительный этап - связка с графическим интерфейсом и настройка стилизации

--- JavaScript Edition/decide.js
+++ JavaScript Edition/decide.js
@@ -1,5 +1,8 @@
+let chk = 0;
+let sw = true;
 function calc(example){
 	let prioritet = new Map();
+	let slices = [];
 	function get_active(){
 		let string = '';
 		for(let index = prioritet.get('op_index_active')+1; index <= example.length-1; index++){
@@ -57,15 +60,57 @@ function calc(example){
 			return (+prioritet.get('passive')) - (+prioritet.get('active'));
 		};
 	};
-	function search(op1, op2){
+	//pt = ParenThese
+	//op = OPerator
+	//st = STart
+	//en = ENd
+	//chk = CHecK
+	function get_pt(){
+		let indl = new Array();
+		for(let i = 0; i <= example.length-1; i++){
+			//console.log('ptindex: ' + i); //
+			if(example[i]=='('){
+				indl.push(i);
+			};
+			if(example[i]==')'){return {st: indl.pop()+1, end: i}};
+			if(example[i]==')'&&i==example.length-1){return {st: indl.pop(), end: i}};
+		};
+	};
+	function search_pt(pt){
+		let counter = -1;
+		let char = '';
+		for(i in example){
+			counter += 1;
+			char = example.charAt(i);
+			if(char==pt){
+				let obj_pt = get_pt();
+				//console.log(obj_pt)
+				try{
+					prioritet.set('pt_st_index', obj_pt.st);
+					prioritet.set('pt_en_index', obj_pt.end);
+				}catch(err){return};
+				//prioritet.set('parethese', pt);
+				prioritet.set('part', example.slice(prioritet.get('pt_st_index'), prioritet.get('pt_en_index')));
+				slices.push(example);
+				example = prioritet.get('part')
+				let result = calc(example);
+				example = slices.pop();
+				example = example.slice(0, prioritet.get('pt_st_index')-1)+result+example.slice(prioritet.get('pt_en_index')+1, example.length);
+				if(example.includes('(')){search_pt(pt)};
+				//console.log(prioritet); //
+				//console.log('example: ' + example);
+			};
+			
+		};
+	};
+	function search(op){
 		let counter = -1;
 		let symbol;
 		for(i in example){
 			counter += 1;
 			symbol = example.charAt(i);
-			if(symbol==op1 || symbol==op2){
-				if(symbol==op1){prioritet.set('operator', op1);};
-				if(symbol==op2){prioritet.set('operator', op2);};
+			if(symbol==op){ 
+				prioritet.set('operator', op);
 				prioritet.set('op_index_active', counter);
 				prioritet.set('op_index_passive', counter);
 				prioritet.set('active', get_active());
@@ -73,7 +118,7 @@ function calc(example){
 				//console.log(example) //
 				example = example.slice(0, prioritet.get('st_index'))+decide()+example.slice(prioritet.get('en_index')-1, example.length);
 				//console.log(prioritet) //
-				if(example.includes(op1) || example.includes(op2)){search(op1, op2);};
+				if(example.includes(op)){search(op);};
 				break;
 			};
 		};
@@ -94,9 +139,16 @@ function calc(example){
 			};
 		};
 	};
-	double_search('**')
-	search('*', '/');
-	search('+', '-');
-	return example
+	search_pt('(');
+	double_search('**');
+	search('*');
+	search('/');
+	search('-');
+	search('+');
+	return example;
 };
-console.log(calc('2**3**2/4+2')); //
+//5*(5*(2+2)) //100
+//4+(6*2)**2+(7-6) //149
+
+
+
